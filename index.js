c31e7aa411fab0a8f1d2d98b1364b50c1d752e74
tringuyenminh209 <tringuyenminh209@gmail.com>
2025-04-25 14:24:27 +0900

    update index.js

--- index.js
+++ index.js
@@ -7,7 +7,22 @@ const port = 3000;
 
 
 // Firebaseの構成ファイルをインポート
-const serviceAccount = require("./firebase-config.json");
+// const serviceAccount = require("./firebase-config.json");
+require("dotenv").config(); 
+
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+};
 
 // Firebase Adminの初期化
 admin.initializeApp({
